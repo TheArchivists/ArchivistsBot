1040da95c05d9308a533d87a15ffe0a94df3941c
TsunayoshiSawada <animekaizokux@gmail.com>
2021-05-30 10:19:42 +0530

    Added /list command back along with /search cause prev guy was high

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,7 +1,7 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.ListCommand = 'search'
+        self.ListCommand = ["list","search"]
         self.AuthorizedUsersCommand = 'users'
         self.AuthorizeCommand = 'auth'
         self.UnAuthorizeCommand = 'unauth'
