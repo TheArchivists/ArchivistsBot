fefdf260c20e52884a981cab0c320ba6cdc4a2b7
Hentai Ousama <kutabare.eerito@gmail.com>
2021-09-25 01:06:40 +0530

    Removed Redundancy from filters.py and updated the filters used.

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -1,24 +1,9 @@
-from telegram.ext import BaseFilter
+from telegram.ext import Filters
 
 from bot import AUTHORIZED_CHATS, OWNER_ID
 
 
 class CustomFilters:
-    class _OwnerFilter(BaseFilter):
-        def filter(self, message):
-            return bool(message.from_user.id == OWNER_ID)
-
-    owner_filter = _OwnerFilter()
-
-    class _AuthorizedUserFilter(BaseFilter):
-        def filter(self, message):
-            id = message.from_user.id
-            return bool(id in AUTHORIZED_CHATS or id == OWNER_ID)
-
-    authorized_user = _AuthorizedUserFilter()
-
-    class _AuthorizedChat(BaseFilter):
-        def filter(self, message):
-            return bool(message.chat.id in AUTHORIZED_CHATS)
-
-    authorized_chat = _AuthorizedChat()
+    owner_filter = Filters.user(user_id=OWNER_ID)
+    authorized_user = owner_filter | Filters.user(user_id=AUTHORIZED_CHATS)
+    authorized_chat = owner_filter | Filters.chat(chat_id=AUTHORIZED_CHATS)
